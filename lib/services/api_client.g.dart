1117a5b40427d8c7c6c51a59944232a360a5716e
Janakiram <palepujanakiram@gmail.com>
2026-03-20 15:03:19 +0530

    Included the app to make /api/settings api call

--- lib/services/api_client.g.dart
+++ lib/services/api_client.g.dart
@@ -49,6 +49,33 @@ class _ApiClient implements ApiClient {
   }
 
   @override
+  Future<AppSettingsModel> getAppSettings() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<AppSettingsModel>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/settings',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late AppSettingsModel _value;
+    try {
+      _value = AppSettingsModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
   Future<List<int>> transformImage(
     String prompt,
     String negativePrompt,
