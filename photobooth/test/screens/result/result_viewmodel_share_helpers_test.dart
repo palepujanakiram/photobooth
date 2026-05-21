64ab69d498c12ec060109f36675d251292e2767c
Janakiram Palepu <janakiram.palepu@prasthana.com>
2026-05-21 20:25:08 +0530

    Fixed Github Action Failure

--- photobooth/test/screens/result/result_viewmodel_share_helpers_test.dart
+++ photobooth/test/screens/result/result_viewmodel_share_helpers_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:photobooth/screens/result/kiosk_receipt_share_fallback.dart';
 import 'package:photobooth/screens/result/result_viewmodel_share_helpers.dart';
+import 'package:photobooth/utils/constants.dart';
+import 'package:photobooth/utils/exceptions.dart';
 
 void main() {
   test('applyKioskFallbackWhenReceiptShareEmpty no-op when receipt URL set', () {
@@ -62,5 +64,54 @@ void main() {
       ),
     );
     expect(receiptUrl, '');
+  });
+
+  test('shareGeneratedImageUrlsOnWeb returns message when urls empty', () async {
+    final message = await shareGeneratedImageUrlsOnWeb(
+      urls: const [],
+      shareText: (_, {subject, sharePositionOrigin}) async {},
+    );
+    expect(message, 'No images to share');
+  });
+
+  test('shareGeneratedImageUrlsOnWeb joins urls and shares', () async {
+    String? sharedText;
+    String? sharedSubject;
+    final message = await shareGeneratedImageUrlsOnWeb(
+      urls: const ['https://a.jpg', 'https://b.jpg'],
+      shareText: (text, {sharePositionOrigin, String? subject}) async {
+        sharedText = text;
+        sharedSubject = subject;
+      },
+    );
+    expect(message, isNull);
+    expect(sharedText, 'https://a.jpg\nhttps://b.jpg');
+    expect(sharedSubject, '${AppConstants.kBrandName} photos');
+  });
+
+  test('shareGeneratedImageUrlsOnWeb maps ShareException to user message', () async {
+    final message = await shareGeneratedImageUrlsOnWeb(
+      urls: const ['https://a.jpg'],
+      shareText: (_, {subject, sharePositionOrigin}) async {
+        throw ShareException('unsupported');
+      },
+    );
+    expect(
+      message,
+      anyOf(
+        'Sharing not supported in this browser. Link copied.',
+        'Failed to share',
+      ),
+    );
+  });
+
+  test('shareGeneratedImageUrlsOnWeb maps unexpected errors', () async {
+    final message = await shareGeneratedImageUrlsOnWeb(
+      urls: const ['https://a.jpg'],
+      shareText: (_, {subject, sharePositionOrigin}) async {
+        throw Exception('network');
+      },
+    );
+    expect(message, 'Failed to share: Exception: network');
   });
 }
